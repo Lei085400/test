32c134fc278f8f539596957013bc0e2cb02808c7
Your Name <you@example.com>
2023-12-19 19:33:13 +0800

    remove mathlib lib

--- lakefile.lean
+++ lakefile.lean
@@ -5,8 +5,8 @@ package «lean4-example» {
   -- add package configuration options here
 
 }
-require mathlib from
-  git "https://github.com/leanprover-community/mathlib4" @ "b6ec7450650a5945bf4244751be4a5cf1fee962f"
+-- require mathlib from
+--   git "https://github.com/leanprover-community/mathlib4" @ "b6ec7450650a5945bf4244751be4a5cf1fee962f"
 
 -- require mathlib from
 --   git "https://github.com/leanprover-community/mathlib4"
