ce4917c555fa194454b8ce05432caeedbc3561d7
Your Name <you@example.com>
2023-12-19 17:28:06 +0800

    remove namespace

--- Lean4Example.lean
+++ Lean4Example.lean
@@ -2,11 +2,9 @@
 import Mathlib.Data.Nat.Choose.Basic
 import Mathlib.Data.Nat.ModEq
 
-def hello := "world"
-
 open Nat
 
-namespace exp_test
+-- namespace exp_test
 
 --定理1.2
 theorem test12(h1:1 ≤ n)(h2:1 ≤ k) : choose n k = choose (n-1) k  + choose (n-1) (k-1) := by
@@ -18,4 +16,4 @@ theorem test12(h1:1 ≤ n)(h2:1 ≤ k) : choose n k = choose (n-1) k  + choose (
    rw[Nat.sub_add_cancel h2]
   rw[hk, choose_succ_succ]
 
-end exp_test
+-- end exp_test
