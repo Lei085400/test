f348be5a69181ce98cff3230f8287534306337f8
Your Name <you@example.com>
2024-01-08 17:52:54 +0800

    123

--- Lean4Example.lean
+++ Lean4Example.lean
@@ -38,12 +38,12 @@ theorem idt1_Pascal's_Recurrence(h1:1 ≤ n)(h2:1 ≤ k) : choose n k = choose (
 
 
 
---rw [add_comm]对应生成的新定理
--- theorem BDA2 (h1:1 ≤ n)(h2:1 ≤ k) (h0:choose n k = choose (n-1) k  + choose (n-1) (k-1)):
---  choose (1 + (n - 1)) (k - 1 + 1) = choose (n - 1) (k - 1) + choose (n - 1) k := by
---    have choose_eq_choose_sub_add :  choose n k = choose (n - 1 + 1) (k - 1 + 1)  := by
---     rw[Nat.sub_add_cancel h1, Nat.sub_add_cancel h2]
---    rw [← add_comm]
---    rw [← add_comm (choose (n - 1) k) (choose (n - 1) (k - 1))]
---    rw [← choose_eq_choose_sub_add]
---    rw [h0]
+rw [add_comm]对应生成的新定理
+theorem BDA2 (h1:1 ≤ n)(h2:1 ≤ k) (h0:choose n k = choose (n-1) k  + choose (n-1) (k-1)):
+ choose (1 + (n - 1)) (k - 1 + 1) = choose (n - 1) (k - 1) + choose (n - 1) k := by
+   have choose_eq_choose_sub_add :  choose n k = choose (n - 1 + 1) (k - 1 + 1)  := by
+    rw[Nat.sub_add_cancel h1, Nat.sub_add_cancel h2]
+   rw [← add_comm]
+   rw [← add_comm (choose (n - 1) k) (choose (n - 1) (k - 1))]
+   rw [← choose_eq_choose_sub_add]
+   rw [h0]
